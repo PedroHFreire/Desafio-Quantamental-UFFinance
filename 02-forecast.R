32a4f3c7634885bfef0284640491765a5990a887
PedroHFreire <63743569+PedroHFreire@users.noreply.github.com>
2020-08-11 08:42:18 -0300

    Update 02-forecast.R
    - Código foi refeito, agora usando metodologia de criar o intervalo de previsão através da previsão de modelos selecionados
    
    - Ainda falta transformar em função
    
    - o código 01 e este podem ser combinados para virarem um só, talvez isso simplifique e ajude a otimizar

--- 02-forecast.R
+++ 02-forecast.R
@@ -1,85 +1,53 @@
-# Previsão out-of-sample -------------------------------------------------- 
-# Vai começar com os últimos 4 anos da primeira amostra de 5 anos
-# Então vai prever um dia a frente, tirar o primeiro dia da amostra e adicionar o retorno realizado em d+1
-
-setwd("D:/Google Drive/Desafio Quantamental/GARCH Vol. forecast/Scripts")
-source("01-model-selection.R")
-
-returns <- PerformanceAnalytics::CalculateReturns(prices, method = "log")[-1] 
-colnames(returns) <- "BOVA11"
-
-# Start (iteração 1)
-returns_first <- first(returns, '5 years')
-returns_subset <- last(returns_first, '4 years')
-
-
-garch_fit <- ugarchfit(spec = garch_spec, returns_subset)
-
-garch_boot <- rugarch::ugarchboot(garch_fit,
-                                  data = returns_subset,
-                                  method = "Partial",
-                                  n.ahead = 1,
-                                  n.bootpred = 10000)
-
-vol_forecast <- rugarch::sigma(garch_boot@forc)
-
-day_one <- which(index(returns) == index(first(returns_subset))) + 1 # Posição do segundo dia em "returns"
-day_ahead <- which(index(returns) == index(last(returns_subset))) + 1 # posição do D + 1 em "returns"
-
-
-# Outras iterações
-
-# Rodar um for aqui para rodar a seleção a cada trimestre
-forecast_subset <- returns[paste(index(returns[day_one]), "/")]
-
-
-# Rodar um for aqui para cada dia de um trimestre
-
-quarters <- endpoints(forecast_subset, on = "quarters")[-1]
-quarters <- quarters[-length(quarters)]
-
-
-j <- 1
-for(quarter in quarters) {
-  print(paste(j, quarter))
-  quarter_subset <- forecast_subset[j:quarter]
-  j <- quarter + 1
+#rollingForecast <- function(returns, chosen_specs) {
+  cluster <- parallel::makeCluster(parallel::detectCores() - 1)
+  parallel::clusterExport(cluster, varlist = c("chosen_specs",
+                                               "returns"))
   
-  for(i in 1:length(quarter_subset)) {
-    returns_subset <- returns[day_one:day_ahead]
+  
+  init_smpl_size <- length(xts::first(returns, '2 years'))
+  
+  chosen_rolls <- lapply(chosen_specs,
+                         FUN = rugarch::ugarchroll,
+                         data = returns,
+                         n.start = init_smpl_size,
+                         refit.window = "moving",
+                         refit.every = 1,
+                         calculate.VaR = FALSE,
+                         keep.coef = TRUE,
+                         cluster = cluster)
+  
+  parallel::stopCluster(cluster)
+  
+  
+  chosen_rolls <- chosen_rolls[sapply(chosen_rolls, rugarch::convergence) != 1]# Tirando modelos que nao convergiram EDIT colocar trycatch pq se tiver janela que nao converge dá ruim, pode usar o método resume para re-estimar também
+  
+  
+  for (roll in chosen_rolls) {
+    sigma <- as.data.frame(roll)$Sigma
     
-    garch_fit <- ugarchfit(spec = garch_spec, returns_subset)
-    
-    garch_boot <- rugarch::ugarchboot(garch_fit,
-                                      data = returns_subset,
-                                      method = "Partial",
-                                      n.ahead = 1,
-                                      n.bootpred = 10000)
-    
-    vol_forecast <- rugarch::sigma(garch_boot@forc)
-    
-    if(!exists("boot_pred_interval")){
-      boot_pred_interval <- cbind(t(as.data.frame(garch_boot,
-                                                  which = "sigma",
-                                                  type = "summary")),
-                                  vol_forecast)
+    if(!exists("pred_interval")){
+      pred_interval <- sigma
       
     } else {
-      boot_pred_interval <- rbind(boot_pred_interval,
-                                  cbind(t(as.data.frame(garch_boot,
-                                                        which = "sigma",
-                                                        type = "summary")),
-                                        vol_forecast))
+      pred_interval <- cbind(pred_interval, sigma)
     }
-    
-    day_one <- which(index(returns) == index(first(returns_subset))) + 1
-    day_ahead <- which(index(returns) == index(last(returns_subset))) + 1
     
   }
   
-  # re-select model
-}
-
-# se der erro lança um trycatch para usar o melhor modelo anterior a esse
-
-
+  colnames(pred_interval) <- names(chosen_rolls)
+  
+  
+  pred_output <- cbind(apply(pred_interval, 1, min),
+                       apply(pred_interval, 1, max))
+  
+  colnames(pred_output) <- c("Min pred.", "Max pred.")
+  
+  # Output
+  # EDIT  numero do semestre
+  out <- list("Intervalo de previsao"= xts::xts(pred_output, order.by = as.Date(rownames(as.data.frame(roll)))),
+              "Modelos escolhidos" = names(chosen_rolls))
+  
+#  return(out)
+#}
+# Falta adicionar a re-selecao
+# Vale comparar esses acima via MSE do garch roll aqui no forecast (real out of sample)
